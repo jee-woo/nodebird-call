7ec6a97d581b17d1dcc4012bd30cb2e011e3c86e
callmejeje <choije401@naver.com>
2023-03-25 18:38:17 +0900

    feat: v2 client

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
+const URL = process.env.API_URL;
+axios.defaults.headers.origin = process.env.ORIGIN;
+// 요즘엔 common 넣는게 맞지만 없어도 동작함
+// axios.defaults.headers.common.origin = 'http://localhost:4000';
 
+/*
 exports.test = async (req, res, next) => {
   try {
     if (!req.session.jwt) {
@@ -28,4 +33,53 @@ exports.test = async (req, res, next) => {
     }
     return next(error);
   }
+};
+*/
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token;
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    });
+  } catch (error) {
+    if (error.response?.status === 419) {
+      // 토큰의 유효기간이 지난 경우
+      delete req.session.jwt;
+      return request(req, api); // 재귀 함수로 호출하면 토큰이 없어서 다시 발급 받게 됨
+    }
+    return error.response;
+  }
+};
+
+exports.getMyPosts = async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+  try {
+    const result = await request(
+      req,
+      `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}` // 한글 때문에 encodeURIComponent로 감싸줌
+    );
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.renderMain = (req, res) => {
+  res.render("main", { key: process.env.CLIENT_SECRET });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { test } = require("../controllers");
+const { getMyPosts, searchByHashtag, renderMain } = require("../controllers");
 const router = express.Router();
 
-router.get("/test", test);
+router.get("/myposts", getMyPosts);
+router.get("/search/:hashtag", searchByHashtag);
+router.get("/", renderMain);
 
 module.exports = router;
